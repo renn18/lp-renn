4adcf98863bd31f88b0ad967db4583d79f00af41
Renn <ppskdos@gmail.com>
2024-07-08 21:16:54 +0800

    fixed mobile view

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -72,26 +72,26 @@ export const projects = [
 
 function About() {
   return (
-    <div className='grid grid-cols-1 px-40 py-40 gap-5'>
-        {/* <h1 className='font-bold text-5xl mt-40'>Hello</h1> */}
-        <div className='text-center'>
-        <h1 className='font-bold text-6xl py-3'>Skills</h1>
+    <div className='grid grid-cols-1 max-w-7xl mx-auto py-20 md:py-40 px-4'>
+      {/* <h1 className='font-bold text-5xl mt-40'>Hello</h1> */}
+      <div className='text-center'>
+        <h1 className='text-4xl md:text-7xl font-bold text-white py-3'>Skills</h1>
         <p className='text-lg'>includes framework, plugin, etc</p>
         <HoverEffect items={projects} />
-        <div className='flex justify-center gap-4'>
+        <div className='flex justify-center gap-4 '>
           <div className='flex gap-4 m-0 py-3 px-5 rounded-full bg-black'>
-        <Image src={nexticon} alt='react' width={32} height={32} className='opacity-25 border rounded-full'/>
-        <Image src={reacticon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={typescripticon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={nodejsicon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={phpicon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={figmaicon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={pyicon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={javaicon} alt='react' width={32} height={32} className='opacity-25'/>
-        <Image src={postgreicon} alt='react' width={32} height={32} className='opacity-25'/>
+            <Image src={nexticon} alt='react' width={32} height={32} className='opacity-25 border rounded-full' />
+            <Image src={reacticon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={typescripticon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={nodejsicon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={phpicon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={figmaicon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={pyicon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={javaicon} alt='react' width={32} height={32} className='opacity-25' />
+            <Image src={postgreicon} alt='react' width={32} height={32} className='opacity-25' />
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </div>
   )
 }
